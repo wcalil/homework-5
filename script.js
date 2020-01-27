ed3398becf8274ac4fc00be6f76fd177b7213221
Wilson Calil <wilson.calil@outlook.com>
2020-01-26 18:07:00 -0600

    upload of the complete version of Javascript code with the local storage working.

--- script.js
+++ script.js
@@ -108,22 +108,142 @@ switch (currentHourM) {
 // To local storage
 
 
-
-var storedToDo9am = " ";
-var ToDos9am = $("#9");
+// 9 am
 
 $("#save9am").click(function(){
     var comment = $.trim($("#9").val());
     if(comment != ""){
         // alert(comment);
-        var storedToDo9am = localStorage.setItem("ToDoAt9", JSON.stringify(comment));
+        localStorage.setItem("ToDoAt9", JSON.stringify(comment));
         alert("Saved!")
     }
-
     
 });
 
-    $("#9").val(localStorage.getItem("ToDoAt9"))
+$("#9").val(localStorage.getItem("ToDoAt9"))
+
+
+// 10 am
+
+$("#save10am").click(function(){
+    var comment = $.trim($("#10").val());
+    if(comment != ""){
+        // alert(comment);
+        localStorage.setItem("ToDoAt10", JSON.stringify(comment));
+        alert("Saved!")
+    }
+    
+});
+
+$("#10").val(localStorage.getItem("ToDoAt10"))
+
+
+// 11 am
+
+$("#save11am").click(function(){
+    var comment = $.trim($("#11").val());
+    if(comment != ""){
+        // alert(comment);
+        localStorage.setItem("ToDoAt11", JSON.stringify(comment));
+        alert("Saved!")
+    }
+    
+});
+
+$("#11").val(localStorage.getItem("ToDoAt11"))
+
+
+// 12 pm
+
+$("#save12pm").click(function(){
+    var comment = $.trim($("#12").val());
+    if(comment != ""){
+        // alert(comment);
+        localStorage.setItem("ToDoAt12", JSON.stringify(comment));
+        alert("Saved!")
+    }
+    
+});
+
+$("#12").val(localStorage.getItem("ToDoAt12"))
+
+
+// 1 pm
+
+$("#save1pm").click(function(){
+    var comment = $.trim($("#1").val());
+    if(comment != ""){
+        // alert(comment);
+        localStorage.setItem("ToDoAt1", JSON.stringify(comment));
+        alert("Saved!")
+    }
+    
+});
+
+$("#1").val(localStorage.getItem("ToDoAt1"))
+
+// 2 pm
+
+$("#save2pm").click(function(){
+    var comment = $.trim($("#2").val());
+    if(comment != ""){
+        // alert(comment);
+        localStorage.setItem("ToDoAt2", JSON.stringify(comment));
+        alert("Saved!")
+    }
+    
+});
+
+$("#2").val(localStorage.getItem("ToDoAt2"))
+
+ 
+// 3 pm
+
+$("#save3pm").click(function(){
+    var comment = $.trim($("#3").val());
+    if(comment != ""){
+        // alert(comment);
+        localStorage.setItem("ToDoAt3", JSON.stringify(comment));
+        alert("Saved!")
+    }
+    
+});
+
+$("#3").val(localStorage.getItem("ToDoAt3"))
+
+
+// 4 pm
+
+$("#save4pm").click(function(){
+    var comment = $.trim($("#4").val());
+    if(comment != ""){
+        // alert(comment);
+        localStorage.setItem("ToDoAt4", JSON.stringify(comment));
+        alert("Saved!")
+    }
+    
+});
+
+$("#4").val(localStorage.getItem("ToDoAt4"))
+
+
+// 5 pm
+
+$("#save5pm").click(function(){
+    var comment = $.trim($("#5").val());
+    if(comment != ""){
+        // alert(comment);
+        localStorage.setItem("ToDoAt5", JSON.stringify(comment));
+        alert("Saved!")
+    }
+    
+});
+
+$("#5").val(localStorage.getItem("ToDoAt5"))
+
+
+
 
 
 })
+
