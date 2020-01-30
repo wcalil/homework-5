20f6ffb86bd6db08c016f7eab12091268d423189
Wilson Calil <wilson.calil@outlook.com>
2020-01-29 21:51:05 -0600

    Uploading a more DRY code with a loop replacing the switch condition for changing the text area background color

--- script.js
+++ script.js
@@ -1,267 +1,202 @@
-
-$(document).ready(function(){
-
-// To imput the current date on the First block of the page
-
-var currentDate = $("#currentDate");
-// console.log(currentDate);
-
-var date = new Date();
-// console.log(date)
-
-var dateM = moment(date).format("dddd, MMMM Do YYYY")
-currentDate.text(dateM)
+$(document).ready(function () {
 
 
-// To get the divs to change color after time is past
+    // To imput the current date on the First block of the page
 
-var currentHourM = moment(date).format("hA")
-console.log(currentHourM)
+    var currentDate = $("#currentDate");
+    // console.log(currentDate);
 
-switch (currentHourM) {
+    var date = new Date();
+    // console.log(date)
 
-    case "9AM":
-        $("#9").css("background-color", "rgb(80, 255, 255)") ;
-        $("#9").html(" ") ;
-        $("#9").css("placeholder", "") ;
-        break;
-    case "10AM":
-        $("#9").css("background-color", "rgb(27, 104, 104)") ;
-        $("#9").html(" ") ;
-        $("#10").css("background-color", "rgb(80, 255, 255)") ;
-        break;
-    case "11AM":
-        $("#9").css("background-color", "rgb(27, 104, 104)") ;
-        $("#9").html(" ") ;
-        $("#10").css("background-color", "rgb(27, 104, 104)") ;
-        $("#11").css("background-color", "rgb(80, 255, 255)") ;
-        break;
-    case "12PM":
-        $("#9").css("background-color", "rgb(27, 104, 104)") ;
-        $("#9").html(" ") ;
-        $("#10").css("background-color", "rgb(27, 104, 104)") ;
-        $("#11").css("background-color", "rgb(27, 104, 104)") ;
-        $("#12").css("background-color", "rgb(80, 255, 255)") ;
-        break;
-    case "1PM":
-        $("#9").css("background-color", "rgb(27, 104, 104)") ;
-        $("#9").html(" ") ;
-        $("#10").css("background-color", "rgb(27, 104, 104)") ;
-        $("#11").css("background-color", "rgb(27, 104, 104)") ;
-        $("#12").css("background-color", "rgb(27, 104, 104)") ;
-        $("#1").css("background-color", "rgb(80, 255, 255)") ;
-        break;
-    case "2PM":
-        $("#9").css("background-color", "rgb(27, 104, 104)") ;
-        $("#9").html(" ") ;
-        $("#10").css("background-color", "rgb(27, 104, 104)") ;
-        $("#11").css("background-color", "rgb(27, 104, 104)") ;
-        $("#12").css("background-color", "rgb(27, 104, 104)") ;
-        $("#1").css("background-color", "rgb(27, 104, 104)") ;
-        $("#2").css("background-color", "rgb(80, 255, 255)") ;
-        break;
-    case "3PM":
-        $("#9").css("background-color", "rgb(27, 104, 104)") ;
-        $("#9").html(" ") ;
-        $("#10").css("background-color", "rgb(27, 104, 104)") ;
-        $("#11").css("background-color", "rgb(27, 104, 104)") ;
-        $("#12").css("background-color", "rgb(27, 104, 104)") ;
-        $("#1").css("background-color", "rgb(27, 104, 104)") ;
-        $("#2").css("background-color", "rgb(27, 104, 104)") ;
-        $("#3").css("background-color", "rgb(80, 255, 255)") ;
-        break;
-    case "4PM":
-        $("#9").css("background-color", "rgb(27, 104, 104)") ;
-        $("#9").html(" ") ;
-        $("#10").css("background-color", "rgb(27, 104, 104)") ;
-        $("#11").css("background-color", "rgb(27, 104, 104)") ;
-        $("#12").css("background-color", "rgb(27, 104, 104)") ;
-        $("#1").css("background-color", "rgb(27, 104, 104)") ;
-        $("#2").css("background-color", "rgb(27, 104, 104)") ;
-        $("#3").css("background-color", "rgb(27, 104, 104)") ;
-        $("#4").css("background-color", "rgb(80, 255, 255)") ;
-        break;
-    case "5PM":
-        $("#9").css("background-color", "rgb(27, 104, 104)") ;
-        $("#9").html(" ") ;
-        $("#10").css("background-color", "rgb(27, 104, 104)") ;
-        $("#11").css("background-color", "rgb(27, 104, 104)") ;
-        $("#12").css("background-color", "rgb(27, 104, 104)") ;
-        $("#1").css("background-color", "rgb(27, 104, 104)") ;
-        $("#2").css("background-color", "rgb(27, 104, 104)") ;
-        $("#3").css("background-color", "rgb(27, 104, 104)") ;
-        $("#4").css("background-color", "rgb(27, 104, 104)") ;
-        $("#5").css("background-color", "rgb(80, 255, 255)") ;
-        break;
-    case "6PM":
-    case "7PM":
-    case "8PM":
-    case "9PM": 
-    case "10PM":
-    case "11PM":
-        $("#9").css("background-color", "rgb(27, 104, 104)") ;
-        $("#9").html(" ") ;
-        $("#10").css("background-color", "rgb(27, 104, 104)") ;
-        $("#11").css("background-color", "rgb(27, 104, 104)") ;
-        $("#12").css("background-color", "rgb(27, 104, 104)") ;
-        $("#1").css("background-color", "rgb(27, 104, 104)") ;
-        $("#2").css("background-color", "rgb(27, 104, 104)") ;
-        $("#3").css("background-color", "rgb(27, 104, 104)") ;
-        $("#4").css("background-color", "rgb(27, 104, 104)") ;
-        $("#5").css("background-color", "rgb(27, 104, 104)") ;
-    
-  }
+    var dateM = moment(date).format("dddd, MMMM Do YYYY")
+    currentDate.text(dateM)
 
 
-// To local storage
+    // To get the divs to change color after time is past
 
+    var area = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+    // console.log(area)
 
-// 9 am
+    var hourIndex = parseInt(moment(date).format("H"));
+    console.log(hourIndex)
 
-$("#save9am").click(function(){
-    var comment = $.trim($("#9").val());
-    if(comment != ""){
-        // alert(comment);
-        localStorage.setItem("ToDoAt9", JSON.stringify(comment));
-        alert("Saved!")
+    for (i = 0; i < area.length; i++) {
+
+        var areaNumber = parseInt(area[i]);
+        console.log("#" + areaNumber)
+
+        if (hourIndex >= 18 && hourIndex <= 23) {
+            $("#" + areaNumber).css("background-color", "rgb(27, 104, 104)");
+
+        }
+
+        else if (areaNumber < hourIndex && areaNumber >= 9 && areaNumber <= 17) {
+            $("#" + areaNumber).css("background-color", "rgb(27, 104, 104)");
+
+        }
     }
-    
-});
 
-$("#9").val(localStorage.getItem("ToDoAt9"))
+    for (i = 0; i < area.length; i++) {
 
+        var areaNumber = parseInt(area[i]);
 
-// 10 am
+        if (areaNumber == hourIndex) {
+            $("#" + areaNumber).css("background-color", "rgb(80, 255, 255)");
+            $("#" + areaNumber).text("Start writing your ToDo's in the light green spots...");
 
-$("#save10am").click(function(){
-    var comment = $.trim($("#10").val());
-    if(comment != ""){
-        // alert(comment);
-        localStorage.setItem("ToDoAt10", JSON.stringify(comment));
-        alert("Saved!")
+        }
     }
-    
-});
 
-$("#10").val(localStorage.getItem("ToDoAt10"))
+    // To local storage
 
+    // 9 am
 
-// 11 am
+    $("#save9am").click(function () {
+        var comment = $.trim($("#9").val());
+        if (comment != "") {
+            // alert(comment);
+            localStorage.setItem("ToDoAt9", JSON.stringify(comment));
+            alert("Saved!")
+        }
 
-$("#save11am").click(function(){
-    var comment = $.trim($("#11").val());
-    if(comment != ""){
-        // alert(comment);
-        localStorage.setItem("ToDoAt11", JSON.stringify(comment));
-        alert("Saved!")
-    }
-    
-});
+    });
 
-$("#11").val(localStorage.getItem("ToDoAt11"))
+    $("#9").val(localStorage.getItem("ToDoAt9"))
 
 
-// 12 pm
+    // 10 am
 
-$("#save12pm").click(function(){
-    var comment = $.trim($("#12").val());
-    if(comment != ""){
-        // alert(comment);
-        localStorage.setItem("ToDoAt12", JSON.stringify(comment));
-        alert("Saved!")
-    }
-    
-});
+    $("#save10am").click(function () {
+        var comment = $.trim($("#10").val());
+        if (comment != "") {
+            // alert(comment);
+            localStorage.setItem("ToDoAt10", JSON.stringify(comment));
+            alert("Saved!")
+        }
 
-$("#12").val(localStorage.getItem("ToDoAt12"))
+    });
 
-
-// 1 pm
-
-$("#save1pm").click(function(){
-    var comment = $.trim($("#1").val());
-    if(comment != ""){
-        // alert(comment);
-        localStorage.setItem("ToDoAt1", JSON.stringify(comment));
-        alert("Saved!")
-    }
-    
-});
-
-$("#1").val(localStorage.getItem("ToDoAt1"))
-
-// 2 pm
-
-$("#save2pm").click(function(){
-    var comment = $.trim($("#2").val());
-    if(comment != ""){
-        // alert(comment);
-        localStorage.setItem("ToDoAt2", JSON.stringify(comment));
-        alert("Saved!")
-    }
-    
-});
-
-$("#2").val(localStorage.getItem("ToDoAt2"))
-
- 
-// 3 pm
-
-$("#save3pm").click(function(){
-    var comment = $.trim($("#3").val());
-    if(comment != ""){
-        // alert(comment);
-        localStorage.setItem("ToDoAt3", JSON.stringify(comment));
-        alert("Saved!")
-    }
-    
-});
-
-$("#3").val(localStorage.getItem("ToDoAt3"))
+    $("#10").val(localStorage.getItem("ToDoAt10"))
 
 
-// 4 pm
+    // 11 am
 
-$("#save4pm").click(function(){
-    var comment = $.trim($("#4").val());
-    if(comment != ""){
-        // alert(comment);
-        localStorage.setItem("ToDoAt4", JSON.stringify(comment));
-        alert("Saved!")
-    }
-    
-});
+    $("#save11am").click(function () {
+        var comment = $.trim($("#11").val());
+        if (comment != "") {
+            // alert(comment);
+            localStorage.setItem("ToDoAt11", JSON.stringify(comment));
+            alert("Saved!")
+        }
 
-$("#4").val(localStorage.getItem("ToDoAt4"))
+    });
+
+    $("#11").val(localStorage.getItem("ToDoAt11"))
 
 
-// 5 pm
+    // 12 pm
 
-$("#save5pm").click(function(){
-    var comment = $.trim($("#5").val());
-    if(comment != ""){
-        // alert(comment);
-        localStorage.setItem("ToDoAt5", JSON.stringify(comment));
-        alert("Saved!")
-    }
-    
-});
+    $("#save12pm").click(function () {
+        var comment = $.trim($("#12").val());
+        if (comment != "") {
+            // alert(comment);
+            localStorage.setItem("ToDoAt12", JSON.stringify(comment));
+            alert("Saved!")
+        }
 
-$("#5").val(localStorage.getItem("ToDoAt5"))
+    });
 
-//Clear Scheduler
+    $("#12").val(localStorage.getItem("ToDoAt12"))
 
-$(".clearButton").click(function(){
-    var confirmV = confirm("Are you sure you want to delete your Todos?")
-    
-    switch (confirmV){
-    case true:
-    localStorage.clear();
-    location.reload(true);
 
-}
+    // 1 pm
 
-})
+    $("#save1pm").click(function () {
+        var comment = $.trim($("#1").val());
+        if (comment != "") {
+            // alert(comment);
+            localStorage.setItem("ToDoAt1", JSON.stringify(comment));
+            alert("Saved!")
+        }
+
+    });
+
+    $("#1").val(localStorage.getItem("ToDoAt1"))
+
+
+    // 2 pm
+
+    $("#save2pm").click(function () {
+        var comment = $.trim($("#2").val());
+        if (comment != "") {
+            // alert(comment);
+            localStorage.setItem("ToDoAt2", JSON.stringify(comment));
+            alert("Saved!")
+        }
+
+    });
+
+    $("#2").val(localStorage.getItem("ToDoAt2"))
+
+
+    // 3 pm
+
+    $("#save3pm").click(function () {
+        var comment = $.trim($("#3").val());
+        if (comment != "") {
+            // alert(comment);
+            localStorage.setItem("ToDoAt3", JSON.stringify(comment));
+            alert("Saved!")
+        }
+
+    });
+
+    $("#3").val(localStorage.getItem("ToDoAt3"))
+
+
+    // 4 pm
+
+    $("#save4pm").click(function () {
+        var comment = $.trim($("#4").val());
+        if (comment != "") {
+            // alert(comment);
+            localStorage.setItem("ToDoAt4", JSON.stringify(comment));
+            alert("Saved!")
+        }
+
+    });
+
+    $("#4").val(localStorage.getItem("ToDoAt4"))
+
+
+    // 5 pm
+
+    $("#save5pm").click(function () {
+        var comment = $.trim($("#5").val());
+        if (comment != "") {
+            // alert(comment);
+            localStorage.setItem("ToDoAt5", JSON.stringify(comment));
+            alert("Saved!")
+        }
+
+    });
+
+    $("#5").val(localStorage.getItem("ToDoAt5"))
+
+    //Clear Scheduler
+
+    $(".clearButton").click(function () {
+        var confirmV = confirm("Are you sure you want to delete your Todos?")
+
+        switch (confirmV) {
+            case true:
+                localStorage.clear();
+                location.reload(true);
+
+        }
+
+    })
 
 
 })
